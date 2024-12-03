6e5997ad9c3b1df43cb9b638a1707c9653a5790b
oksmith <ollie.smith@ravelin.com>
2024-12-03 12:53:02 +0000

    Day 3 part 2

--- day3/03_solution.go
+++ day3/03_solution.go
@@ -12,7 +12,7 @@ import (
 
 func main() {
 	fmt.Printf("Part 1 result: %d\n", partone())
-	// fmt.Printf("Part 2 result: %d\n", parttwo())
+	fmt.Printf("Part 2 result: %d\n", parttwo())
 }
 
 func partone() int {
@@ -23,15 +23,44 @@ func partone() int {
 
 	result := 0
 	for _, match := range matches {
-		// take the firstNumber,secondNumber substring
-		subStr := match[4 : len(match)-1]
-
-		digitsStr := strings.Split(subStr, ",")
-		firstNumber, _ := strconv.Atoi(digitsStr[0])
-		secondNumber, _ := strconv.Atoi(digitsStr[1])
-		result += firstNumber * secondNumber
+		product := mul(match)
+		result += product
 	}
 	return result
+}
+
+func parttwo() int {
+	data := readInput()
+
+	// look for all `mul` and `do` and `undo` operations
+	r := regexp.MustCompile(`mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)`)
+	matches := r.FindAllString(data, -1)
+
+	result := 0
+	do := true
+	for _, match := range matches {
+		if match == "do()" {
+			do = true
+		} else if match == "don't()" {
+			do = false
+		} else {
+			if do {
+				product := mul(match)
+				result += product
+			}
+		}
+	}
+	return result
+}
+
+func mul(matchString string) int {
+	// take the firstNumber,secondNumber substring
+	subStr := matchString[4 : len(matchString)-1]
+
+	digitsStr := strings.Split(subStr, ",")
+	firstNumber, _ := strconv.Atoi(digitsStr[0])
+	secondNumber, _ := strconv.Atoi(digitsStr[1])
+	return firstNumber * secondNumber
 }
 
 func readInput() string {
