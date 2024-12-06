bb70ca84bd414d795a19c25768334810baf06cde
oksmith <ollie.smith@ravelin.com>
2024-12-06 15:36:14 +0000

    Day 6 part 2

--- day6/06_solution.go
+++ day6/06_solution.go
@@ -5,17 +5,19 @@ import (
 	"os"
 	"path"
 	"runtime"
+	"strconv"
 	"strings"
 )
 
 type Rules [100][100]bool
 
 func main() {
-	fmt.Printf("Part 1 result: %d\n", partone())
-	// fmt.Printf("Part 2 result: %d\n", parttwo())
+	partOne, partTwo := solution()
+	fmt.Printf("Part 1 result: %d\n", partOne)
+	fmt.Printf("Part 2 result: %d\n", partTwo)
 }
 
-func partone() int {
+func solution() (int, int) {
 	data := parseInput()
 
 	directions := map[int][2]int{
@@ -28,23 +30,67 @@ func partone() int {
 
 	// find starting position
 	x, y := findStartPos(data)
-	// fmt.Println(x, y)
 
 	// initialise visitedLocations with the starting position
 	visitedLocations := map[string]bool{
 		fmt.Sprintf("%d,%d", x, y): true,
 	}
 	for x > 0 && x < len(data)-1 && y > 0 && y < len(data)-1 {
-		x, y, direction = move(data, x, y, direction, directions, visitedLocations)
-		// fmt.Println("direction", direction, "which is", directions[direction][0], directions[direction][1])
-		// fmt.Println(x, y)
+		x, y, direction = move(data, x, y, direction, directions)
+		visitedLocations[fmt.Sprintf("%d,%d", x, y)] = true
 	}
-	return len(visitedLocations)
+
+	// we can use the visitedLocations from part 1 to find the
+	// potential loops in part 2. Only consider visited locations
+	// when choosing potential obstructions
+	// loop over all possible obstruction options
+	x, y = findStartPos(data)
+	direction = 0
+	loops := 0
+	for loc, _ := range visitedLocations {
+		locSplit := strings.Split(loc, ",")
+		i, _ := strconv.Atoi(locSplit[0])
+		j, _ := strconv.Atoi(locSplit[1])
+		if i != x || j != y {
+			// create a new map with the obstruction
+			var newData [][]string
+			for k, row := range data {
+				newRow := make([]string, len(row))
+				copy(newRow, row)
+				if k == i {
+					newRow[j] = "#"
+				}
+				newData = append(newData, newRow)
+			}
+
+			isLoop := isLoop(newData, x, y, direction, directions)
+			if isLoop {
+				loops++
+			}
+		}
+	}
+	return len(visitedLocations), loops
 }
 
-func move(data [][]string, x, y, direction int, directions map[int][2]int, visitedLocations map[string]bool) (int, int, int) {
+func isLoop(data [][]string, x, y, direction int, directions map[int][2]int) bool {
+	// initialise visitedLocationDirections with the starting position and direction
+	visitedLocationDirections := map[string]bool{
+		fmt.Sprintf("%d,%d,%d", x, y, direction): true,
+	}
+	for x > 0 && x < len(data)-1 && y > 0 && y < len(data)-1 {
+		x, y, direction = move(data, x, y, direction, directions)
+		if visitedLocationDirections[fmt.Sprintf("%d,%d,%d", x, y, direction)] {
+			return true
+		} else {
+			visitedLocationDirections[fmt.Sprintf("%d,%d,%d", x, y, direction)] = true
+		}
+	}
+	return false
+
+}
+
+func move(data [][]string, x, y, direction int, directions map[int][2]int) (int, int, int) {
 	if data[x+directions[direction][0]][y+directions[direction][1]] == "#" {
-		// fmt.Println("identified obstruction at", x+directions[direction][0], y+directions[direction][1])
 		// if the next position in current direction is an obstruction
 		// move direction 90 degrees and do nothing else
 		direction = (direction + 1) % 4
@@ -53,8 +99,6 @@ func move(data [][]string, x, y, direction int, directions map[int][2]int, visit
 		// we move in the current direction and append the new position to visitedLocations
 		x += directions[direction][0]
 		y += directions[direction][1]
-		visitedLocations[fmt.Sprintf("%d,%d", x, y)] = true
-		// fmt.Println("moved to", x, y)
 	}
 	return x, y, direction
 }
